17432ee29570d2559000bee84c9070c395743d68
Ricardo J. Barberis <ricardo.barberis@donweb.com>
2018-12-11 22:49:15 -0300

    Initial commit, based on Vincent Batts' excellent work: https://github.com/vbatts/slackware-docker

---
+++ mkimage-slackware32.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+# Generate a very minimal filesystem from slackware
+
+export LANG=C
+
+set -e
+
+if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH="" ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=64 ;;
+  esac
+fi
+
+BUILD_NAME=${BUILD_NAME:-"slackware"}
+#VERSION=${VERSION:="current"}
+VERSION="14.2"
+#RELEASENAME=${RELEASENAME:-"slackware${ARCH}"} #"
+RELEASENAME="slackware"
+RELEASE=${RELEASE:-"${RELEASENAME}-${VERSION}"} #"
+#MIRROR=${MIRROR:-"http://slackware.osuosl.org"}
+MIRROR="http://mirrors.us.kernel.org/slackware/"
+CACHEFS=${CACHEFS:-"/tmp/${BUILD_NAME}/${RELEASE}"} #"
+ROOTFS=${ROOTFS:-"/tmp/rootfs-${RELEASE}"} #"
+CWD=$(pwd)
+
+base_pkgs="a/aaa_base \
+	a/aaa_elflibs \
+	a/coreutils \
+	a/glibc-solibs \
+	a/aaa_terminfo \
+	a/pkgtools \
+	a/shadow \
+	a/tar \
+	a/xz \
+	a/bash \
+	a/etc \
+	a/gzip \
+	n/wget \
+	n/gnupg \
+	a/elvis \
+	ap/slackpkg \
+	l/ncurses \
+	a/bin \
+	a/bzip2 \
+	a/grep \
+	a/sed \
+	a/dialog \
+	a/file \
+	a/gawk \
+	a/time \
+	a/gettext \
+	a/libcgroup \
+	a/patch \
+	a/sysfsutils \
+	a/time \
+	a/tree \
+	a/utempter \
+	a/which \
+	a/util-linux \
+	l/mpfr \
+	l/libunistring \
+	ap/diffutils \
+	a/procps \
+	n/net-tools \
+	a/findutils \
+	n/iproute2 \
+	n/openssl"
+
+function cacheit() {
+	file=$1
+	if [ ! -f "${CACHEFS}/${file}"  ] ; then
+		mkdir -p $(dirname ${CACHEFS}/${file})
+		echo "Fetching $file" 1>&2
+		curl -s -o "${CACHEFS}/${file}" "${MIRROR}/${RELEASE}/${file}"
+	fi
+	echo "/cdrom/${file}"
+}
+
+mkdir -p $ROOTFS $CACHEFS
+
+cacheit "isolinux/initrd.img"
+
+cd $ROOTFS
+# extract the initrd to the current rootfs
+## ./slackware64-14.2/isolinux/initrd.img:    gzip compressed data, last modified: Fri Jun 24 21:14:48 2016, max compression, from Unix, original size 68600832
+## ./slackware64-current/isolinux/initrd.img: XZ compressed data
+if $(file ${CACHEFS}/isolinux/initrd.img | grep -wq XZ) ; then
+	xzcat "${CACHEFS}/isolinux/initrd.img" | cpio -idvm --null --no-absolute-filenames
+else
+	zcat "${CACHEFS}/isolinux/initrd.img" | cpio -idvm --null --no-absolute-filenames
+fi
+
+if stat -c %F $ROOTFS/cdrom | grep -q "symbolic link" ; then
+	rm $ROOTFS/cdrom
+fi
+mkdir -p $ROOTFS/{mnt,cdrom,dev,proc,sys}
+
+for dir in cdrom dev sys proc ; do
+	if mount | grep -q $ROOTFS/$dir  ; then
+		umount $ROOTFS/$dir
+	fi
+done
+
+mount --bind $CACHEFS ${ROOTFS}/cdrom
+mount -t devtmpfs none ${ROOTFS}/dev
+mount --bind -o ro /sys ${ROOTFS}/sys
+mount --bind /proc ${ROOTFS}/proc
+
+mkdir -p mnt/etc
+cp etc/ld.so.conf mnt/etc
+
+relbase=$(echo ${RELEASE} | cut -d- -f1)
+if [ ! -f ${CACHEFS}/paths ] ; then
+	ruby ${CWD}/get_paths.rb ${RELEASE} > ${CACHEFS}/paths
+fi
+for pkg in ${base_pkgs}
+do
+	path=$(grep ^${pkg} ${CACHEFS}/paths | cut -d : -f 1)
+	if [ ${#path} -eq 0 ] ; then
+		echo "$pkg not found"
+		continue
+	fi
+	l_pkg=$(cacheit $relbase/$path)
+	if [ -e ./sbin/installpkg ] ; then
+		PATH=/bin:/sbin:/usr/bin:/usr/sbin \
+		chroot . /sbin/installpkg --root /mnt --terse ${l_pkg}
+	else
+		PATH=/bin:/sbin:/usr/bin:/usr/sbin \
+		chroot . /usr/lib/setup/installpkg --root /mnt --terse ${l_pkg}
+	fi
+done
+
+cd mnt
+set -x
+touch etc/resolv.conf
+echo "export TERM=linux" >> etc/profile.d/term.sh
+chmod +x etc/profile.d/term.sh
+echo ". /etc/profile" > .bashrc
+echo "${MIRROR}/${RELEASE}/" >> etc/slackpkg/mirrors
+sed -i 's/DIALOG=on/DIALOG=off/' etc/slackpkg/slackpkg.conf
+sed -i 's/POSTINST=on/POSTINST=off/' etc/slackpkg/slackpkg.conf
+sed -i 's/SPINNING=on/SPINNING=off/' etc/slackpkg/slackpkg.conf
+
+mount --bind /etc/resolv.conf etc/resolv.conf
+chroot . sh -c 'yes y | linux32 /usr/sbin/slackpkg -batch=on -default_answer=y update'
+chroot . sh -c 'linux32 /usr/sbin/slackpkg -batch=on -default_answer=y upgrade-all'
+
+# now some cleanup of the minimal image
+set +x
+#rm -rf var/lib/slackpkg/*
+#rm -rf usr/share/locale/*
+#rm -rf usr/man/*
+#find usr/share/terminfo/ -type f ! -name 'linux' -a ! -name 'xterm' -a ! -name 'screen.linux' -exec rm -f "{}" \;
+umount $ROOTFS/dev
+rm -f dev/* # containers should expect the kernel API (`mount -t devtmpfs none /dev`)
+umount etc/resolv.conf
+
+tar --numeric-owner -cf- . > ${CWD}/${RELEASE}.tar
+ls -sh ${CWD}/${RELEASE}.tar
+
+for dir in cdrom dev sys proc ; do
+	if mount | grep -q $ROOTFS/$dir  ; then
+		umount $ROOTFS/$dir
+	fi
+done
+
+
